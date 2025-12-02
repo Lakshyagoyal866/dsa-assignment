56d09a91b57b85b69c2c2210cc0ed8b8d6124b10
Lakshyagoyal866 <lakshyagoyal584@gmail.com>
2025-12-03 04:53:06 +0530

    Update q3.cpp

--- assignment9/q3.cpp
+++ assignment9/q3.cpp
@@ -5,47 +5,38 @@
 
 using namespace std;
 
-// Structure to represent an edge in the graph
 struct Edge {
-    int u, v; // Endpoints of the edge
+    int u, v; 
     int weight;
 };
 
-// Custom comparator for sorting edges by weight (used for Kruskal's)
 bool compareEdges(const Edge& a, const Edge& b) {
     return a.weight < b.weight;
 }
 
-// --- Disjoint Set Union (DSU) Implementation ---
-
-// Parent array for DSU. parent[i] stores the parent of node i.
 vector<int> parent;
 
-// Find operation with Path Compression
+
 int findSet(int i) {
     if (parent[i] == i)
         return i;
-    return parent[i] = findSet(parent[i]); // Path compression
+    return parent[i] = findSet(parent[i]); 
 }
 
-// Union operation by Rank (or just simple union)
 void unionSets(int i, int j) {
     int root_i = findSet(i);
     int root_j = findSet(j);
     if (root_i != root_j) {
-        // Simple union: make one root the parent of the other
         parent[root_i] = root_j;
     }
 }
 
-// --- Kruskal's Algorithm ---
 
 void kruskalMST(int numNodes, vector<Edge>& edges) {
-    // 1. Initialize DSU structure
     parent.resize(numNodes);
-    iota(parent.begin(), parent.end(), 0); // Fill parent[i] = i (each node is its own parent)
+    iota(parent.begin(), parent.end(), 0); 
 
-    // 2. Sort all edges by weight
+    
     sort(edges.begin(), edges.end(), compareEdges);
 
     vector<Edge> mstEdges;
@@ -54,19 +45,16 @@ void kruskalMST(int numNodes, vector<Edge>& edges) {
 
     cout << "Edges selected for MST (Kruskal's):\n";
 
-    // 3. Iterate through all sorted edges
     for (const auto& edge : edges) {
-        // Check if adding this edge creates a cycle (i.e., if u and v are already in the same set)
         if (findSet(edge.u) != findSet(edge.v)) {
-            // No cycle, add the edge to MST
             mstEdges.push_back(edge);
             mstWeight += edge.weight;
-            unionSets(edge.u, edge.v); // Merge the two sets
+            unionSets(edge.u, edge.v); 
             edgesCount++;
             
             cout << "Edge: " << edge.u << " - " << edge.v << " (Weight: " << edge.weight << ")\n";
 
-            // Optimization: MST is complete when it has (numNodes - 1) edges
+       
             if (edgesCount == numNodes - 1) {
                 break;
             }
@@ -77,7 +65,7 @@ void kruskalMST(int numNodes, vector<Edge>& edges) {
 }
 
 int main() {
-    // Graph with 6 nodes (0 to 5)
+
     int numNodes = 6; 
     vector<Edge> edges = {
         {0, 1, 4}, {0, 2, 4}, {1, 2, 2},
